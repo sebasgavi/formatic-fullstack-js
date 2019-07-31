380e953b717fb2cdd27f640c71fbd7f94a7ddaa6
Sebastian Gaviria <vagyriag@hotmail.com>
2019-07-30 21:01:33 -0500

    commit inicial, clase 2

---
+++ main.js
@@ -0,0 +1,34 @@
+function ocultar(elemento){
+    //var elemento = document.querySelector('.card');
+    elemento.setAttribute('hidden', true);
+}
+
+
+
+var cards = document.querySelectorAll('.card');
+cards.forEach(function(card, index){
+    var btn = card.querySelector('.btn');
+    btn.addEventListener('click', function(){
+        //ocultar(card);
+        var popup = document.querySelector('.popup');
+        //console.log();
+        popup.removeAttribute('hidden');
+        //popup.innerHTML = '<h1>hola ' + (index+1) + '</h1>';
+        
+        var titulo = document.createElement('h1');
+        titulo.innerText = card.getAttribute('data-titulo');
+        
+        titulo.addEventListener('click', function(){
+            this.remove();
+        });
+
+        popup.appendChild(titulo);
+
+        card.appendChild(titulo);
+
+        
+    });
+});
+
+
+
