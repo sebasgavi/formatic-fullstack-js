d986682073084082db6025f23f9e3df4854bc191
Sebastián Gaviria <sebastiangaviria@outlook.com>
2019-07-31 21:21:50 -0500

    adding, checking and removing items

--- main.js
+++ main.js
@@ -1,30 +1,52 @@
-function ocultar(elemento){
-    //var elemento = document.querySelector('.card');
-    elemento.setAttribute('hidden', true);
-}
+window.addEventListener('load', () => {
 
+  const app = document.querySelector('#app');
+  const input = app.querySelector('input');
+  const list = app.querySelector('.list-group');
 
+  var elements = [];
 
-var cards = document.querySelectorAll('.card');
-cards.forEach(function(card, index){
-    var btn = card.querySelector('.btn');
-    btn.addEventListener('click', function(){
-        //ocultar(card);
-        var popup = document.querySelector('.popup');
-        //console.log();
-        popup.removeAttribute('hidden');
-        //popup.innerHTML = '<h1>hola ' + (index+1) + '</h1>';
-        
-        var titulo = document.createElement('h1');
-        titulo.innerText = card.getAttribute('data-titulo');
-        
-        titulo.addEventListener('click', function(){
-            this.remove();
-        });
-        popup.appendChild(titulo);
-        
+  // auto enfocar input
+  input.focus();
+
+  input.addEventListener('keydown', ({ keyCode, target }) => {
+    if(keyCode === 13){
+      elements.push({
+        text: target.value,
+        checked: false,
+      });
+      target.value = '';
+      render();
+    }
+  });
+
+  function render(){
+    list.innerHTML = '';
+    list.append(...elements.map(getTask));
+  }
+
+  function getTask(elem, index) {
+    const { text, checked } = elem;
+    var item = document.createElement('div');
+    item.setAttribute('class', 'list-group-item');
+    item.innerHTML = `
+      <label>
+        <input type="checkbox" ${checked ? 'checked' : ''} />
+        ${text}
+      </label>
+      <span class="badge badge-danger float-right">x</span>
+    `;
+    item.querySelector('label').addEventListener('click', (e) => {
+      e.preventDefault();
+      let checkbox = item.querySelector('input');
+      elem.checked = !checkbox.checked;
+      render();
     });
+    item.querySelector('.badge').addEventListener('click', (e) => {
+      elements.splice(index, 1);
+      render();
+    });
+    return item;
+  }
+
 });
-
-
-
