33eee417ecb50b7685910d7f2282ff7a4e405181
Sebastian Gaviria <vagyriag@hotmail.com>
2019-08-06 18:56:33 -0500

    search input

--- main.js
+++ main.js
@@ -3,21 +3,83 @@ window.addEventListener('load', () => {
     // thecatapi.com
     var breeds;
 
-    var getBtn = document.querySelector('#get-cats');
-    getBtn.addEventListener('click', () => {
-        api.getBreeds()
-            .then(data => {
-                console.log(data);
-                breeds = data;
-                setCatElements();
+    api.getBreeds()
+        .then(data => {
+            breeds = data;
+            breeds.push({
+                name: 'Criollo Alemán',
+                origin: 'Cali',
+                weight: {
+                    metric: '5',
+                }
             });
-    });
+            breeds.push({
+                name: 'Gato Cansón',
+                origin: 'Cali',
+                weight: {
+                    metric: '5',
+                }
+            });
+            breeds.push({
+                name: 'Brasileño áíóÖ',
+                origin: 'Cali',
+                weight: {
+                    metric: '5',
+                }
+            });
+            breeds = breeds.map(cat => {
+                return { 
+                    ...cat,
+                    normalized: _.deburr(cat.name),
+                };
+            });
+            setCatElements(breeds);
+        });
 
-    function setCatElements(){
-        var row = document.querySelector('.row');
-        breeds.forEach(obj => {
+    var modified = debounce(() => {
+        if(input.value.length < 3){
+            setCatElements(breeds);
+        } else {
+            var filtered = breeds.filter(({ normalized, origin }) => {
+                let reg = new RegExp(_.deburr(input.value), 'i');
+                return reg.test(normalized) || reg.test(origin);
+            });
+            setCatElements(filtered, input.value);
+        }
+    }, 300);
+
+    var input = document.querySelector('#search-input');
+    input.addEventListener('input', modified);
+
+    function debounce(otherFunction, timer){
+        var iden;
+
+        function timeout() {
+            clearTimeout(iden);
+            iden = setTimeout(() => {
+                otherFunction();
+            }, timer);
+        }
+
+        return timeout;
+    }
+
+
+    function setCatElements(list, search){
+        var row = document.querySelector('.row.cats-grid');
+        row.innerHTML = '';
+        list.forEach(obj => {
             row.append( getCatElement(obj, openCatModal) );
         });
+        if(list.length === 0){
+            row.innerHTML = `
+            <div class="col mt-4">
+                <div class="alert alert-info">
+                    Lo sentimos, no encontramos gatos para la búsqueda <strong>${search}</strong>
+                </div>
+            </div>
+            `;
+        }
     }
 
     function openCatModal(obj){
